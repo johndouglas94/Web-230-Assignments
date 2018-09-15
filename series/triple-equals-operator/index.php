2826735809f22a29991e7de2c2d4db4e8db1977f
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-09-15 19:41:23 -0400

    Add files via upload

---
+++ series/triple-equals-operator/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$num1 = '1';
+$num2 = 1;
+
+if($num1==$num2){
+  echo 'Equal.';
+} else{
+  echo 'Not equal.';
+}
+
+echo '<br/ >';
+
+if($num1===$num2){
+  echo 'Equal.';
+} else{
+  echo 'Not equal';
+}
+
+?>
