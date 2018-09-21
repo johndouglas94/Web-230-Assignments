f2f8941c8dd5bad5b7698f3b6451f40df1f5f8cd
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-09-21 13:02:48 -0400

    Add files via upload

---
+++ series/string-functions-2/index.php
@@ -0,0 +1,15 @@
+<?php
+
+  $string = 'This is an example string.';
+  $string_reversed = strrev($string);
+
+  echo $string_reversed;
+
+  echo '<br/ >';
+
+  $string2 = 'Hi my name is John.';
+  $string_shuffled = str_shuffle($string2);
+
+  echo $string_shuffled;
+
+?>
