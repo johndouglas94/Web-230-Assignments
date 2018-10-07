a3546dd339a750208405dbedb0c9f6df1f2dec3a
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-10-07 00:21:53 -0400

    Add files via upload

---
+++ series/strings-position/index.php
@@ -0,0 +1,16 @@
+<?php
+
+  $offset = 0;
+
+  $find = 'is';
+  $find_length = strlen($find);
+
+  $string = 'This is a sting,  and it is an example.';
+
+  while ($string_position = strpos($string, $find, $offset)) {
+    echo '<strong>'.$find.'</strong> found at '.$string_position.'<br>';
+    $offset = $string_position + $find_length;
+  }
+
+
+?>
