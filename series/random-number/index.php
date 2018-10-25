f15ec21f132204375facaf7d05f65879fa5f3d5f
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-10-25 17:40:39 -0400

    Update index.php

--- series/random-number/index.php
+++ series/random-number/index.php
@@ -1,9 +1,14 @@
 <?php
 
+1. What the if staement is doing here is determining that if the "Roll dice" button is been clicked, echo out what is
+ below. 
 if (isset($_POST['roll'])) {
+2. The rand function is a generator that displays random numbers. 
   $rand = rand(1, 6);
   echo 'You rolled a '.$rand;
 }
+
+3. A way that $rand might be used in a web application is if a site uses captcha to block out spammers.  
 
 ?>
 
