9cd53b282ea94b59041fa04dc6b3cd662127b3de
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-10-26 11:53:27 -0400

    Update index.php

--- series/obstart/index.php
+++ series/obstart/index.php
@@ -1,4 +1,7 @@
 <?php
+
+1. We use ob_start because their is an error on the redirect. There is not supposed to be page content before the header 
+   function. ob_start stores the page content in an internal buffer and runs the redirect.
 ob_start();
 ?>
 
@@ -10,9 +13,11 @@ $redirect_page = 'http://google.com';
 $redirect = true;
 
 if ($redirect==true) {
+   2. The header function is using the $redirect_page variable and redirecting the user to google.com. 
    header('Location: '.$redirect_page);
  }
 
+3. We use ob_end_flush to put the page contents back in to the page. Even if we can not redirect.   
 ob_end_flush();
 
 ?>
