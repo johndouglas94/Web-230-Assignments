c58669195f06238039ea3ff7a4c04db33d0f6fe1
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-10-26 14:07:54 -0400

    Update index.php

--- series/get-ip-better/index.php
+++ series/get-ip-better/index.php
@@ -9,6 +9,8 @@ echo '<h3>$http_x_forwarded_for is: '.$http_x_forwarded_for.'</h3>';
 $remote_addr = $_SERVER['REMOTE_ADDR'];
 echo '<h3>$remote_addr is: '.$remote_addr.'</h3>';
 
+1. What this conditional statment is doing is checking to see if either $http_client_ip or $http_x_forwarded_for is
+  NOT empty. If they are NOT empty, display either internet IP or proxy. If they are empty, display local IP address. 
 if (!empty($http_client_ip)) {
   $ip_address = $http_client_ip;
 } else if (!empty($http_x_forwarded_for)) {
