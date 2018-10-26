88337845ed2398e20c7700ae15fd36a8879ff80d
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-10-25 20:14:47 -0400

    Add files via upload

---
+++ series/server-variables/header.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'config.inc.php';
+
+$script_name = $_SERVER['SCRIPT_NAME'];
+
+echo $images; 
+
+ ?>
+
+<form action="<?php echo $script_name; ?>" method="POST">
+<input type="submit" name="submit" value="Submit">
+</form>
---
+++ series/server-variables/index.php
@@ -0,0 +1,13 @@
+<?php
+
+include 'header.inc.php';
+
+if (isset($_POST['submit'])) {
+  echo ' Process 1';
+}
+
+
+
+
+
+?>
