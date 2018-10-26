a0475f538fee885c7cd4143e8e3bef7da5a6f0fe
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-10-26 12:38:17 -0400

    Update index.php

--- series/get-user-ip/index.php
+++ series/get-user-ip/index.php
@@ -2,6 +2,8 @@
 
 require 'config.inc.php';
 
+1. What this foreach loop is doing is going through each of the IP addresses amd seeing if they are blocked from
+  accessing the page. If they are blocked, a messege will appear saying so. 
 foreach($ip_blocked as $ip) {
   if ($ip==$ip_address) {
     die('Your IP address, '.$ip_address.' has been blocked');
