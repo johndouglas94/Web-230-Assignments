1a993137357902a05c8da6c8914cd6512569412e
johndouglas94 <36052460+johndouglas94@users.noreply.github.com>
2018-12-09 22:51:40 -0500

    Add files via upload

---
+++ series/hit-counter/connect.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+echo 'HTTP HOST: '.$_SERVER['HTTP_HOST'].'<br><br>'; 
+
+$conn_error = 'Could not connect.';
+
+if($_SERVER['HTTP_HOST'] === 'localhost'){
+$mysql_host = 'localhost';
+$mysql_user = 'root';
+$mysql_pass = '';
+$mysql_db = 'a_database';
+}else{
+  $mysql_host = 'localhost';
+  $mysql_user = 'johndouglas_a_db';
+  $mysql_pass = 'raccpass';
+  $mysql_db = 'johndouglas_a_db';
+}
+
+if(!$con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db)) {
+  die($conn_error);
+}
+
+?>
